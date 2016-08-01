dfabcb7803eaac79d379efb85fe794ddadc74578
Ramiro Trejo <trejoramiro1@gmail.com>
2016-08-01 00:19:15 -0500

    Change https to http in visit route.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -27,7 +27,7 @@ class LinksController < ApplicationController
     if @link = Link.find_by(slug: params[:slug])
       @visit = Visit.new(link_id: @link.id, ip_address: request.remote_ip)
       @visit.save
-      redirect_to "https://#{@link.target_url}"
+      redirect_to "http://#{@link.target_url}"
     else
       raise ActionController::RoutingError.new('Not Found')
     end
